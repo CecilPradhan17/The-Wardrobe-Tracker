670c10391d797480acf62229d38e9dbfc6acdbf6
CecilPradhan17 <cecilpradhan99@gmail.com>
2025-07-31 18:29:29 -0500

    completed filterModal with css

---
+++ scripts.js
@@ -0,0 +1,14 @@
+//filter bar 
+const filterBar = document.getElementById("filter-bar");
+const addFilterBtn = document.getElementById("add-filter-btn");
+
+//add filter function 
+const addFilter = () => 
+{
+    
+}
+
+const displayFilterModal = () => 
+{
+    
+}
